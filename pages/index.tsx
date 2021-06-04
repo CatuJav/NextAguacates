2967fad51c7878743e00e4052ff68d00f834503e
Javier <jalcoser1996@gmail.com>
2021-06-04 18:37:22 -0500

    Consumo de api

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    //Llamar a la url para consumir la api
+    window
+      .fetch('api/avo')
+      .then((response) => response.json())
+      .then(
+        //Desectruruamos la info del json
+        ({ data, length }) => {
+          setProductList(data)
+        }
+      )
+  }, [])
+
+  console.log(productList)
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map((product) => {
+        return <div>{product.name}</div>
+      })}
     </div>
   )
 }
